19d3d83b8d07edab9088f770a9b0596726cadb75
alex zhang <gzane24@gmail.com>
2018-03-04 01:24:34 +0800

    fix logic err

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 module.exports = app => {
-  if (!app.router.route('/heath')) return;
+  if (app.router.route('/heath')) return;
 
   app.router.get('/heath', ctx => {
     ctx.body = 'hi~, i\'m ok';
